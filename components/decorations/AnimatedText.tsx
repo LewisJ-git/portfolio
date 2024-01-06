d332d0dda7c189fc17147fd267c91d362c4ff9f4
LongNguyen-git <lucasnguyen2112@gmail.com>
2024-01-05 20:39:40 -0600

    removing legacy code

--- components/decorations/AnimatedText.tsx
+++ components/decorations/AnimatedText.tsx
@@ -1,28 +1,7 @@
 import { motion } from "framer-motion";
 
-const AnimatedText = ({
-	text = "",
-	duration = 0,
-	delay = 0,
-	opacity = flicker_immediate(1),
-	initialX = 0,
-	initialY = 0,
-}) => {
-	return (
-		<motion.span
-			variants={animate(
-				initialX,
-				initialY,
-				opacity,
-				0,
-				bezier_curved(duration, delay)
-			)}
-			initial="initial"
-			whileInView="animate"
-			viewport={{ once: true }}>
-			{text}
-		</motion.span>
-	);
+const AnimatedText = () => {
+	return <></>;
 };
 
 export default AnimatedText;
